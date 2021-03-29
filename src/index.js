ed812986755451d8063ac4d61509112e6fdff6f0
xaadrj <saadkhalid616@gmail.com>
2021-03-30 00:18:04 +0500

    React Js JSx

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// Import React and React DOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Create a React component
+const App = () => {
+  return <div> Hi! there</div>;
+};
+
+// Take the react component and show it on the screen
+
+ReactDOM.render(<App />, document.querySelector("#root"));
